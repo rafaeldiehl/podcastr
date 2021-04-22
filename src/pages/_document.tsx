be0660f409b7de8730c6ef740acb92dfdce199e7
Rafael Diehl <rafaeljuliani1984@gmail.com>
2021-04-22 15:05:35 -0300

    style: format with prettier

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,10 @@ export default class myDocument extends Document {
         <Head>
           <meta name="author" content="Rafael Juliani Diehl, Rocketseat" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" /> 
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main></Main>
